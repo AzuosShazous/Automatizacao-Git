e72ea4950af440766723ce0d07b5a8930133321b
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-12-23 17:14:48 -0300

    Atualiza descrição

--- pseudofarm_de_commit.sh
+++ pseudofarm_de_commit.sh
@@ -3,6 +3,10 @@
 # Apesar de o nome do arquivo ser relacionado à farm, na realidade, a ideia é compilar as transições de um arquivo entre as aréas presentes no fluxo git
 # em um único comando. Enfim, nada impede de isto ser utilizado como farm.
 
+# Como usar?
+
+# O 1° parâmetro é o arquivo a ser adicionado no staging area e o 2° parâmetro é a mensagem de commit.
+
 # Parâmetro posicional 1 = Argumento 1 passado pelo utilizador
 git add $1;
 
